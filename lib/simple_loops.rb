11fa56cddb3b29ce615e73176f7158792433f885
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-05 23:14:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,9 +10,12 @@ def loop_message_n_times(message, number)
  number.times { puts message }
 end
 
-def output_array(message, number)
-  
-  
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
 end 
 
 def return_string_array
