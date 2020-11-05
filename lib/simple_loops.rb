67dfdb7dfefab9fc4911c549609b80224f937bda
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-05 22:19:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times(message)
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+end
+ 
+def loop_message_n_times(message, n)
+  
+end
+
+def output_array
+  
+end 
+
+def return_string_array
+  
+end 
