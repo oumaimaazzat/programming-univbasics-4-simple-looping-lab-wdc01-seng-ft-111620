02cd95a8fdd4b2b2dbc0c3773371d3572444ae3d
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-05 23:14:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,6 +18,10 @@ def output_array(array)
   end
 end 
 
-def return_string_array
-  
-end 
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+ 
+    count += 1
+  end
+end
